9826d922f7c3d6bd91a0c66380db7178077d8e49
Jalal azimi <jalalazimi@gmail.com>
2018-04-17 11:56:39 +0430

    Adding files.

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
-const secure = require('express-force-https');
 
 app = express();
 app.use("/", serveStatic ( path.join (__dirname, '/dist') ) )
-app.use(secure)
 app.get('*', function (req, res) {
     res.sendFile(__dirname + '/dist/index.html')
 })
